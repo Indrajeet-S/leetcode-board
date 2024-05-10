a3430fb6605eac5b0fa7b837f54b0e02673123db
QBit <ivs.tech.24@gmail.com>
2024-05-10 18:39:27 +0530

    Mentioned guide name in footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,31 +1,38 @@
-
 export function SiteFooter() {
-    return (
-      <footer className="py-6 md:px-8 md:py-0">
-        <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by {" "}
-            <a
-              href = "https://twitter.com/QBit__"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              QBit
-            </a>
-            . Wait for source code on{" "}
-            <a
-              href="https://github.com/Indrajeet-S"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
-          </p>
-        </div>
-      </footer>
-    )
-  }
-  
+  return (
+    <footer className="py-6 md:px-8 md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Built by{" "}
+          <a
+            href="https://twitter.com/QBit__"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            QBit
+          </a>
+          , mentored by{" "}
+          <a
+            href="https://twitter.com/adaptatron"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Adaptatron
+          </a>
+          . Wait for source code on{" "}
+          <a
+            href="https://github.com/Indrajeet-S"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+}

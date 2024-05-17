f3a4542c8f5dfc8faf4f75cf6db838af4bb0f712
QBit <ivs.tech.24@gmail.com>
2024-05-17 11:54:38 +0530

    Metadata for dynamic route added

--- app/problems/[problemID]/page.tsx
+++ app/problems/[problemID]/page.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next"
 
-export const metadata: Metadata = {
-  title: "Whiteboard - LC Board",
+type Props = {
+  params: {
+    problemID: string
+  }
 }
 
-export default function ProblemID({
-  params,
-}: {
-  params: { problemID: string }
-}) {
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `${params.problemID} | Whiteboard`,
+  }
+}
+
+export default function ProblemID({ params }: Props) {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
       <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-white/25 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">

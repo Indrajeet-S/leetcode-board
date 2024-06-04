0234738bac41b6df7fd33ac8e274f80cb2b491d7
QBit <ivs.tech.24@gmail.com>
2024-06-05 01:35:04 +0530

    Excalidraw customization

--- components/custom/excalidraw-wrapper.tsx
+++ components/custom/excalidraw-wrapper.tsx
@@ -1,18 +1,48 @@
 "use client"
-import { Excalidraw, convertToExcalidrawElements } from "@excalidraw/excalidraw"
+import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw"
+import Link from "next/link"
+import Image from "next/image"
+import * as React from "react"
+import { Main } from "next/document"
 
 // import "@excalidraw/excalidraw/index.css"
 
 const ExcalidrawWrapper: React.FC = () => {
-  // console.info(convertToExcalidrawElements([{
-  //   type: "rectangle",
-  //   id: "rect-1",
-  //   width: 186.47265625,
-  //   height: 141.9765625,
-  // },]));
   return (
     <div style={{ height: "90.5vh", width: "100%" }}>
-      <Excalidraw />
+      <Excalidraw>
+        <MainMenu>
+          <MainMenu.DefaultItems.LoadScene />
+          <MainMenu.DefaultItems.Export />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.Help />
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.DefaultItems.ToggleTheme />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu>
+        <WelcomeScreen>
+          <WelcomeScreen.Hints.MenuHint />
+          <WelcomeScreen.Hints.ToolbarHint />
+          <WelcomeScreen.Center>
+            <WelcomeScreen.Center.Logo>
+              <Image
+                src="/center_logo.jpeg"
+                alt="LC Board"
+                width={200}
+                height={200}
+              />
+            </WelcomeScreen.Center.Logo>
+            <WelcomeScreen.Center.Heading>
+              Notes, Made Simple!
+            </WelcomeScreen.Center.Heading>
+            <WelcomeScreen.Center.Menu>
+              <WelcomeScreen.Center.MenuItemLoadScene />
+              <WelcomeScreen.Center.MenuItemHelp />
+            </WelcomeScreen.Center.Menu>
+          </WelcomeScreen.Center>
+          <WelcomeScreen.Hints.HelpHint />
+        </WelcomeScreen>
+      </Excalidraw>
     </div>
   )
 }

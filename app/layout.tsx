b7e45eec9a52e4597b8f283df2a1de39df26a42f
QBit <ivs.tech.24@gmail.com>
2024-06-25 23:29:05 +0530

    Improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { SiteHeader } from "@/components/site-header"
 import { SiteFooter } from "@/components/site-footer"
+import { Suspense } from "react"
+import Loading from "@/app/loading"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -30,7 +32,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SiteHeader />
-          {children}
+          <Suspense fallback={<Loading />}>{children}</Suspense>
           <SiteFooter />
         </ThemeProvider>
       </body>

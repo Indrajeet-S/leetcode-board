0a0b8777d680deafb4a6432ba43e0eb3376f4727
QBit <ivs.tech.24@gmail.com>
2024-06-25 23:26:27 +0530

    fix loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <div>Hey, it is loading...</div>
+  return (
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <div className="flex justify-center">Hey, it is loading...</div>
+    </div>
+  )
 }

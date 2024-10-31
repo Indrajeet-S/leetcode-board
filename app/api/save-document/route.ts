255cbff9252d4e004bdda8899bb656a82ed463ea
Kelani <kelanialiyu@gmail.com>
2024-10-31 14:20:19 +0100

    clean-up api route fixes issue 1

--- app/api/save-document/route.ts
+++ app/api/save-document/route.ts
@@ -1,19 +1,5 @@
-// app/api/save-document/route.ts
 import { NextResponse } from 'next/server';
-import { initializeApp } from "firebase/app";
-import { getFirestore, } from "firebase/firestore";
 import { saveDocumentData } from "@/lib/firebase/crud"
-
-const firebaseConfig = {
-  apiKey: process.env.NEXTJS_APP_API_KEY,
-  authDomain: process.env.NEXTJS_APP_AUTH_DOMAIN,
-  projectId: "leetcode-board-373e7",
-  storageBucket: "leetcode-board-373e7.appspot.com",
-  messagingSenderId: "219772485394",
-  appId: "1:219772485394:web:4acaa2694ce72639f1fdcb",
-  measurementId: "G-257W47PGQ2",
-};
-
 
 export async function POST(request: Request) {
   const { collectionName, documentId, data } = await request.json();
--- lib/firebase/crud.ts
+++ lib/firebase/crud.ts
@@ -2,6 +2,25 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore, getDoc, doc, setDoc } from "firebase/firestore/lite"
 
+// Web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: `${process.env.NEXT_PUBLIC_APP_API_KEY}`,
+//   authDomain: `${process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN}`,
+//   projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APP_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
+// }
+// const firebaseConfig = {
+//   apiKey: process.env.NEXTJS_APP_API_KEY,
+//   authDomain: process.env.NEXTJS_APP_AUTH_DOMAIN,
+//   projectId: "leetboard-b2be5",
+//   storageBucket: "leetboard-b2be5.appspot.com",
+//   messagingSenderId: "711523201010",
+//   appId: "1:711523201010:web:aa4193f53a03312800056b",
+//   measurementId: "G-9EEPXL30FQ",
+// };
 const firebaseConfig = {
   apiKey: process.env.NEXTJS_APP_API_KEY,
   authDomain: process.env.NEXTJS_APP_AUTH_DOMAIN,
